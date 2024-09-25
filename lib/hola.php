a2ef1a7b1ea2299d8e1a2f1abf2da357320c9418
Kriol368 <andreubeltran2004@gmail.com>
2024-09-25 08:23:13 +0200

    Programa interactivo arreglado

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
 // Autor: Kriol368 <andreubeltran2004@gmail.com>
 print "Introduce tu nombre:";
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
 ?>
